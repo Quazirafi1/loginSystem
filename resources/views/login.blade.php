da2cd4ab693e9b94ad62bb657dfea15cf5a68ca5
Rafi <quazirafi1@gmail.com>
2020-05-14 02:08:41 +0600

    Bootstrap added to login form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,24 +2,59 @@
 <html>
     <head>
         <title>Login Page</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <style type="text/css">
+            <style>
+            .container {
+            height: 200px;
+            position: relative;
+            border: 3px solid green;
+            }
+
+            .vertical-center {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            -ms-transform: translateY(-50%);
+            transform: translateY(-50%);
+            }
+        </style>
+
     </head>
     <body>
-        <h1 align="center">Please Login</h1>
-        <div align= "center">
-        <form method="post" action="{{ url('/login/authenticate') }}">
-            {{ csrf_field() }}
-            <div>
-             <label>Enter Email</label>
-             <input type="email" name="email"/>
-            </div></br>
-            <div>
-             <label>Enter Password</label>
-             <input type="password" name="password"/>
-            </div></br>
-            <div class="form-group">
-             <input type="submit" name="login" value="Login" />
+
+        <div class="container vertical-center container-fluid">
+
+            <div class="row">
+                <div class="offset-md-6 col-md-4">
+                    <h2>Login</h2>
+                    <hr>
+
+                <form method="POST" action="{{ url('/login/authenticate') }}">
+                    {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="">Email</label>
+                            <input type="email" name="email" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Password</label>
+                            <input type="password" name="password" class="form-control">
+                        </div>
+                        <div align="center" class="form-group">
+                            <input type="submit" class="btn btn-primary" name="login" value="Login" />
+                        </div>
+                    </form>
+
+                </div>
             </div>
-           </form>
+
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
     </body>
 </html>
